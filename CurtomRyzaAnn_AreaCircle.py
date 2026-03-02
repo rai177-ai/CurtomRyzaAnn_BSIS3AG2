90a5fdfa8055cf3161d65e69760a757024991286
rai177-ai <raigadicho@gmail.com>
2026-03-02 21:58:15 +0800

    Create CurtomRyzaAnn_AreaCircle.py

---
+++ CurtomRyzaAnn_AreaCircle.py
@@ -0,0 +1,12 @@
+import math
+
+print("Curtom Ryza Ann BSIS 3AG2")
+print("Area of the Circle")
+print("=========================")
+
+radius = float(input("Enter the radius of the circle: "))
+
+area = math.pi * (radius ** 2)
+
+
+print("The area of the circle is: ", area)
