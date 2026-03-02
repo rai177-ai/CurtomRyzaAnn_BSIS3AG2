3972f5e7126659f217c5a9d705ac9e8f648ca3e9
rai177-ai <raigadicho@gmail.com>
2026-03-02 22:08:43 +0800

    Create CurtomRyzaAnn_MultiplyTwoNumbers.py

---
+++ CurtomRyzaAnn_MultiplyTwoNumbers.py
@@ -0,0 +1,8 @@
+print("Ryza Ann Curtom BSIS 3AG2")
+print("Multiply Two Numbers")
+print("=========================")
+
+Ryza = int(input("Enter the first number: "))
+Curtom = int(input("Enter the second number: "))
+
+print("Result: ", Ryza, " x ", Curtom, " = ", Ryza * Curtom)
